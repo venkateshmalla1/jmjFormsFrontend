15aba8aede96e746212ae04ba8c47e05a0479d74
venkateshmalla1 <venky.ece41@gmail.com>
2026-07-13 10:42:55 +0530

    Implement custom sorting for dashboard stats
    Added custom sorting for activity dashboard based on class order and activity name.

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,7 +6,27 @@ function Dashboard() {
 
   useEffect(() => {
     axios.get("https://jmjforms.onrender.com/api/stats")
-      .then(res => setStats(res.data))
+      .then(res => {
+        // Define custom class order
+        const classOrder = ["NURS", "LKG", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+
+        // Sort by class order first, then by activity name alphabetically
+        const sorted = [...res.data].sort((a, b) => {
+          const classIndexA = classOrder.indexOf(a._id.class);
+          const classIndexB = classOrder.indexOf(b._id.class);
+
+          if (classIndexA !== classIndexB) {
+            return classIndexA - classIndexB;
+          }
+
+          // If same class, sort by activity name alphabetically
+          const activityA = a._id.activity || "";
+          const activityB = b._id.activity || "";
+          return activityA.localeCompare(activityB);
+        });
+
+        setStats(sorted);
+      })
       .catch(err => console.error(err));
   }, []);
 
